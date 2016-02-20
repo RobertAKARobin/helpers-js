b6d2e01a7615f1f62e6a974218e310ad5ebdae83
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-20 17:06:03 -0500

    Fixed for node

--- helpers.js
+++ helpers.js
@@ -1,11 +1,15 @@
 "use strict";
 
 var h = (function(){
+  var is_browser = (typeof NodeList !== "undefined");
   return {
     el: el,
     forEach: forEach,
     is_a: is_a,
-    collect: collect
+    collect: collect,
+    is_browser: is_browser,
+    has_html_children: has_html_children,
+    is_html_collection: is_html_collection
   }
   function el(selector, ancestor){
     var out;
@@ -18,12 +22,19 @@ var h = (function(){
     if(typeof prototype !== "undefined") return(input instanceof prototype);
     else return false;
   }
+  function has_html_children(input){
+    if(!is_browser) return false;
+    else return (is_a(input, HTMLElement));
+  }
+  function is_html_collection(input){
+    if(!is_browser) return false;
+    else return (is_a(input, NodeList) || is_a(input, HTMLCollection));
+  }
   function forEach(input, callback){
     var i, l;
-    var isHTMLCollection = (h.is_a(input, NodeList) || h.is_a(input, HTMLCollection));
     if(typeof input == "number") input = new Array(input);
     else if(typeof input == "string") input = input.split("");
-    if((input instanceof Array) || isHTMLCollection){
+    if((input instanceof Array) || is_html_collection(input)){
       l = input.length;
       for(i = 0; i < l; i++) callback(input[i], i);
     }else for(i in input){ callback(input[i], i); }
@@ -31,7 +42,7 @@ var h = (function(){
   function collect(collection, callback){
     var output = [];
     if(!collection) return;
-    if(h.is_a(collection, HTMLElement)) collection = collection.children;
+    if(has_html_children(collection)) collection = collection.children;
     forEach(collection, function(item){
       output.push(callback(item));
     });
