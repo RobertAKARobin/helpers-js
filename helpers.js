a685aa1c2c4ad61bd3787e43201d97abc70bf038
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-23 11:03:43 -0500

    Added AJAX

--- helpers.js
+++ helpers.js
@@ -11,7 +11,30 @@ var h = (function(){
     collect: collect,
     is_browser: is_browser,
     has_html_children: has_html_children,
-    is_html_collection: is_html_collection
+    is_html_collection: is_html_collection,
+    ajax: ajax
+  }
+  function ajax(options, callback){
+    var request = new XMLHttpRequest();
+    var url     = options.url;
+    var method  = (options.method || "GET");
+    var data    = (options.data ? objectToQuery(options.data) : "");
+    var headers = (options.headers || {});
+    request.open(method, url, true);
+    if(!headers["Content-Type"]){
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+    forEach(headers, function(value, key){
+      request.setRequestHeader(key, value);
+    });
+    request.onreadystatechange = function(){
+      var state     = request.readyState;
+      var code      = request.status;
+      var response  = try_json(request.responseText);
+      if(state == 4 && code >= 200 && code < 400) callback(response, request);
+    }
+    request.send(data);
+    return request;
   }
   function collect(collection, callback){
     var output = [];
@@ -49,6 +72,12 @@ var h = (function(){
   function is_html_collection(input){
     if(!is_browser) return false;
     else return (is_a(input, NodeList) || is_a(input, HTMLCollection));
+  }
+  function try_json(string){
+    try{
+      string = JSON.parse(string);
+    }catch(err){}
+    return string;
   }
 })();
 
